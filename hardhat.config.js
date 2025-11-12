d00eadac30943d2d204c1e594d8e5ec38777cd22
ndiritungendo-spec <ndiritungendo@gmail.com>
2025-11-12 13:45:10 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-verify");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     polygon: {
-      url: process.env.POLYGON_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
+      url: process.env.POLYGON_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
-  },
+    apiKey: process.env.POLYGONSCAN_API_KEY || ""
+  }
 };
