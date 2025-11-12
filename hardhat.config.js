c846344786a759ea7899d800cbb19f125ad557ee
ndiritungendo-spec <ndiritungendo@gmail.com>
2025-11-12 13:12:27 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
