157d5b9c1ec3acb9f1d7183e7e3bed9b8cc24496
ndiritungendo-spec <ndiritungendo@gmail.com>
2025-11-12 15:44:05 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,42 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: {
-    version: "0.8.20", // matches your Fluid Token contract
-    settings: {
-      optimizer: {
-        enabled: false, // must be false to match deployed bytecode
-        runs: 200
-      }
-    }
-  },
-
+  solidity: "0.8.20",
   networks: {
     polygon: {
       url: process.env.POLYGON_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
-    },
-    mumbai: {
-      url: process.env.POLYGON_RPC_URL_MUMBAI || process.env.POLYGON_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
     }
   },
-
   etherscan: {
-    apiKey: {
-      polygon: process.env.POLYGONSCAN_API_KEY,
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY
-    }
-  },
-
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-
-  mocha: {
-    timeout: 200000
+    apiKey: process.env.POLYGONSCAN_API_KEY
   }
 };
